784e720712839d598cd45de698bf463a7ef9b60b
ajala ahmad <ahmadajala98@gmail.com>
2022-02-23 04:18:03 +0100

    Sender Repository Interface

---
+++ src/main/java/africa/semicolon/logisticSystem/data/repositories/SenderRepository.java
@@ -0,0 +1,23 @@
+package africa.semicolon.logisticSystem.data.repositories;
+
+import africa.semicolon.logisticSystem.data.models.Sender;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SenderRepository {
+    Sender save(Sender sender);
+
+    Optional<Sender> findSenderByEmail(String email);
+
+    void delete(Sender sender);
+
+    void deleteByEmail(Sender email);
+
+    List<Sender> findAll();
+
+    void deleteAll();
+
+
+}
+;
