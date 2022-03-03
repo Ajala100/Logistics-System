50f4583dfa102edb3044e219cf418022b06a9c3d
ajala ahmad <ahmadajala98@gmail.com>
2022-03-03 05:21:20 +0100

    update

--- src/main/java/africa/semicolon/logisticSystem/services/SenderService.java
+++ src/main/java/africa/semicolon/logisticSystem/services/SenderService.java
@@ -3,13 +3,19 @@ package africa.semicolon.logisticSystem.services;
 import africa.semicolon.logisticSystem.data.models.Sender;
 import africa.semicolon.logisticSystem.dtos.requests.RegisterSenderRequest;
 import africa.semicolon.logisticSystem.dtos.responses.RegisterSenderResponse;
+import africa.semicolon.logisticSystem.exceptions.UserDoesNotExistException;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface SenderService {
     RegisterSenderResponse registerSender(RegisterSenderRequest registerSenderRequest);
 
     List<Sender> getSenders();
+
+    void deleteAllSenders();
+
+    Optional<Sender> findSenderByEmail(String email) throws UserDoesNotExistException;
 
 }
