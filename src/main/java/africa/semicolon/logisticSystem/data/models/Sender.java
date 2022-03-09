8a51ff5057d7b402289671688691ffe976fad664
ajala ahmad <ahmadajala98@gmail.com>
2022-03-09 04:27:02 +0100

    Sender Model update

--- src/main/java/africa/semicolon/logisticSystem/data/models/Sender.java
+++ src/main/java/africa/semicolon/logisticSystem/data/models/Sender.java
@@ -2,6 +2,7 @@ package africa.semicolon.logisticSystem.data.models;
 import lombok.Data;
 @Data
 public class Sender {
+
     private String emailAddress;
     private String senderName;
     private String phoneNumber;
