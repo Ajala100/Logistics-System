baa65b600eb7cc2ad032ac76c848b72578813129
ajala ahmad <ahmadajala98@gmail.com>
2022-03-09 04:55:07 +0100

    Package Repository update

--- src/main/java/africa/semicolon/logisticSystem/data/repositories/PackageRepository.java
+++ src/main/java/africa/semicolon/logisticSystem/data/repositories/PackageRepository.java
@@ -1,6 +1,7 @@
 package africa.semicolon.logisticSystem.data.repositories;
 
 import africa.semicolon.logisticSystem.data.models.Package;
+import africa.semicolon.logisticSystem.exceptions.UserDoesNotExistException;
 
 import java.util.List;
 
@@ -9,6 +10,6 @@ public interface PackageRepository {
     Package save(Package apackage);
     void delete (Package aPackage);
     void delete (Integer id);
-    Package findPackageBySenderEmail(String email);
+    Package findPackageBySenderEmail(String email) throws UserDoesNotExistException;
     Package findPackageById(Integer id);
 }
