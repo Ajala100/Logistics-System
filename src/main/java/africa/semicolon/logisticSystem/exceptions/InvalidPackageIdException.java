c102a72d1ab993863d23f0e8813599c1c4d34429
ajala ahmad <ahmadajala98@gmail.com>
2022-03-11 04:05:57 +0100

    Package service update

--- src/main/java/africa/semicolon/logisticSystem/exceptions/InvalidPackageIdException.java
+++ src/main/java/africa/semicolon/logisticSystem/exceptions/InvalidPackageIdException.java
@@ -1,4 +1,7 @@
 package africa.semicolon.logisticSystem.exceptions;
 
 public class InvalidPackageIdException extends LogisticsAppException{
+    public InvalidPackageIdException(String message){
+        super(message);
+    }
 }
--- src/main/java/africa/semicolon/logisticSystem/services/PackageService.java
+++ src/main/java/africa/semicolon/logisticSystem/services/PackageService.java
@@ -1,10 +1,11 @@
 package africa.semicolon.logisticSystem.services;
 
+import africa.semicolon.logisticSystem.data.models.Package;
 import africa.semicolon.logisticSystem.dtos.requests.AddPackageRequest;
 import africa.semicolon.logisticSystem.dtos.responses.AddPackageResponse;
 import africa.semicolon.logisticSystem.exceptions.UserDoesNotExistException;
 
 public interface PackageService {
     AddPackageResponse addPackage(AddPackageRequest addPackageRequest) throws UserDoesNotExistException;
-    Package findMyPackageWIthMy(Integer id);
+    Package findMyPackageWIthId(Integer id);
 }
