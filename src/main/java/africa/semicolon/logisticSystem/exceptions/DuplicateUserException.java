260555c7ae6374fc780b5f717cdcaba022594c15
ajala ahmad <ahmadajala98@gmail.com>
2022-03-11 04:03:25 +0100

    duplicate user exception

--- src/main/java/africa/semicolon/logisticSystem/exceptions/DuplicateUserException.java
+++ src/main/java/africa/semicolon/logisticSystem/exceptions/DuplicateUserException.java
@@ -1,5 +1,7 @@
 package africa.semicolon.logisticSystem.exceptions;
 
 public class DuplicateUserException extends LogisticsAppException{
-
+    public DuplicateUserException(String message){
+        super(message);
+    }
 }
